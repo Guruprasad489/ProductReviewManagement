9d4755392a8a6fbc97074a19f773158c0621dd99
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-03-06 17:37:10 +0530

    [Guru] Welcome to Product review management program

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to product review management program");
+            Console.ReadLine();
+        }
+    }
+}
